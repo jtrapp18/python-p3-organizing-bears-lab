7e9bf2d59a90072137e39b18223e92784f8ec5f0
+ <jacqueline.trapp18@gmail.com>
2024-12-14 23:12:08 -0500

    lab complete

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES 
+("Mr. Chocolate", 2, "M", "Brown", "Silly", TRUE),
+("Rowdy", 2, "M", "Brown", "Rowdy", TRUE),
+("Tabitha", 2, "F", "Black", "Energentic", FALSE),
+("Sergeant Brown", 2, "M", "Brown", "Silly", TRUE),
+("Melissa", 2, "F", "Brown", "Carefree", TRUE),
+("Grinch", 2, "M", "Green", "Grumpy", FALSE),
+("Wendy", 2, "F", "White", "Relaxed", TRUE),
+(NULL, 2, "F", "Purple", "Sad", FALSE);
